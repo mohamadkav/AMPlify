a8ed5d6fe20de22a47aeb994f444d21d1da505ec
Mohammad <mohamad.kav@gmail.com>
2019-02-14 14:22:51 -0600

    Rendering time added

--- AutoAmp.py
+++ AutoAmp.py
@@ -5,14 +5,18 @@ import codecs
 
 
 save_location="/Users/mohammad/Desktop/"
-web_page="http://www.google.com"
+web_page="http://www.cnn.com"
 
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get(web_page)
+print 'Load time:'
+print driver.execute_script("return new Date().getTime() - performance.timing.navigationStart;")
 with codecs.open(save_location+web_page[web_page.find('//')+2:]+".html", 'w', 'utf-8') as f:
     f.write(driver.page_source)
 driver.get("https://mercury.postlight.com/amp?url="+web_page)
+print 'Load time:'
+print driver.execute_script("return new Date().getTime() - performance.timing.navigationStart;")
 with codecs.open(save_location+web_page[web_page.find('//')+2:]+"_AMP.html", 'w', 'utf-8') as f:
     f.write(driver.page_source)
 driver.close()
